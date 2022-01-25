44bf1a9bf1ca810b4c55f1d9b7d42f561f5efa45
Andre Oliveira <69281348+AndreLPO@users.noreply.github.com>
2022-01-25 20:11:07 -0300

    Aula 2 - Imersão dev

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,9 @@
 //Componente React
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import { useState } from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 function Titulo(props) {
   const Tag = props.tag || "h1";
   return (
@@ -62,17 +34,17 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = "AndreLPO";
-
+  const [username, setUsername] = useState("AndreLPO");
+  const [color, setColor] = useState("#57AE5B");
+  const roteamento = useRouter();
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: color,
           backgroundImage:
             "url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)",
           backgroundRepeat: "no-repeat",
@@ -95,12 +67,16 @@ export default function PaginaInicial() {
             padding: "32px",
             margin: "16px",
             boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            backgroundColor: color,
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              roteamento.push("/chat");
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -116,14 +92,28 @@ export default function PaginaInicial() {
               variant="body3"
               styleSheet={{
                 marginBottom: "32px",
-                color: appConfig.theme.colors.neutrals[300],
+                color: { color },
               }}
             >
               {appConfig.name}
             </Text>
-
+            <input
+              value={color}
+              type="color"
+              onChange={(e) => setColor(e.target.value)}
+            ></input>
+            <Text
+              variant="body3"
+              styleSheet={{
+                marginBottom: "32px",
+                color: appConfig.theme.colors.neutrals[300],
+              }}
+            >
+              {color}
+            </Text>
             <TextField
               fullWidth
+              onChange={(e) => setUsername(e.target.value)}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
@@ -134,6 +124,7 @@ export default function PaginaInicial() {
               }}
             />
             <Button
+              disabled={username.length > 2 ? false : true}
               type="submit"
               label="Entrar"
               fullWidth
@@ -163,13 +154,17 @@ export default function PaginaInicial() {
               minHeight: "240px",
             }}
           >
-            <Image
-              styleSheet={{
-                borderRadius: "50%",
-                marginBottom: "16px",
-              }}
-              src={`https://github.com/${username}.png`}
-            />
+            {username.length >= 2 ? (
+              <Image
+                styleSheet={{
+                  borderRadius: "50%",
+                  marginBottom: "16px",
+                }}
+                src={`https://github.com/${username}.png`}
+              />
+            ) : (
+              <></>
+            )}
             <Text
               variant="body4"
               styleSheet={{
